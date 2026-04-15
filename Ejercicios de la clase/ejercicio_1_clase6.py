138f16e820153d6791e1c14add8644dcaf60f75b
agustin_almuna <agustinalmunax@gmail.com>
2026-04-15 18:15:22 -0300

    ejercicio_1_clase6.py

--- Ejercicios de la clase/ejercicio_1_clase6.py
+++ Ejercicios de la clase/ejercicio_1_clase6.py
@@ -3,7 +3,5 @@ Decimal = 2.15
 Texto = "Hola mundo"
 Verdadero_o_Falso = True
 
-print(f"int: {Entero}")
-print(f"int: {Decimal}")
-print(f"int: {Texto}")
-print(f"int: {Verdadero_o_Falso}")
+
+print(f"int: {Entero}, float: {Decimal}, str: {Texto}, bool: {Verdadero_o_Falso}")
