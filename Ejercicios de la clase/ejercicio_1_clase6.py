017e1a5c41c91e4ab3e228287e1738b04447a833
agustin_almuna <agustinalmunax@gmail.com>
2026-04-15 17:50:44 -0300

    Add files via upload

---
+++ Ejercicios de la clase/ejercicio_1_clase6.py
@@ -0,0 +1,9 @@
+Entero = 12
+Decimal: 2.15
+Texto = "Hola mundo"
+Verdadero_o_Falso = True
+
+print(f"int: {Entero}")
+print(f"int: {Decimal}")
+print(f"int: {Texto}")
+print(f"int: {Verdadero_o_Falso}")
